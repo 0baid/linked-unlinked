cdd9ffb1bef04f8c2d1d6b8879aea6411a88f69d
Obaid <obaidurrehman555@gmail.com>
2021-02-22 17:47:14 +0500

    commit

--- linked.js
+++ linked.js
@@ -1,1 +1,20 @@
-// find all the users that have not been linked
+import {allUsers,linked} from './users.js'
+
+
+
+const sort = () =>{
+
+    // console.log(allUsers)
+    // console.log(linked)
+    let sorted = []
+    for(let i=0;i<allUsers.length;i++){
+        if(allUsers[i].id != linked[0].id && allUsers[i].id != linked[1].id)
+        {
+            sorted.push(allUsers[i])
+        }
+        
+    }
+    console.log(sorted)
+}
+
+sort();
